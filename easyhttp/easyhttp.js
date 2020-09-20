787528c31936db3467973e893b55100246263600
patrick sunday <sundayblinkz1@gmail.com>
2020-09-20 22:54:36 +0100

    update

--- easyhttp/easyhttp.js
+++ easyhttp/easyhttp.js
@@ -1,22 +1,22 @@
-function easyHTTP() {
-  this.http = new XMLHttpRequest();
-}
+// function easyHTTP() {
+//   this.http = new XMLHttpRequest();
+// }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url, callback) {
-  this.http.open('GET', url, true);
+// easyHTTP.prototype.get = function(url, callback) {
+//   this.http.open('GET', url, true);
 
-  let self = this;
-  this.http.onload = function() {
-    if(self.http.status === 200) {
-      callback(null, self.http.responseText);
-    } else {
-      callback('Error: ' + self.http.status);
-    }
-  }
+//   let self = this;
+//   this.http.onload = function() {
+//     if(self.http.status === 200) {
+//       callback(null, self.http.responseText);
+//     } else {
+//       callback('Error: ' + self.http.status);
+//     }
+//   }
 
-  this.http.send();
-}
+//   this.http.send();
+// }
 
 // Make an HTTP POST Request
 
